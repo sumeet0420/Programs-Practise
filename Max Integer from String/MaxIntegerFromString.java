1b72b2b7adeb59d97f9ab29c667beb8c08061844
Sumeet Agrawal <sumeetagrawal840@gmail.com>
2020-03-27 11:53:55 +0530

    Update MaxIntegerFromString.java

--- Max Integer from String/MaxIntegerFromString.java
+++ Max Integer from String/MaxIntegerFromString.java
@@ -4,9 +4,7 @@ public class MaxIntegerFromString {
 
 	public static void main(String[] args) {
 	String x = "132549";
-	System.out.println(Stream.of(x.split(""))
-								  .mapToInt(Integer::parseInt)
-								  .max().getAsInt());
+	System.out.println(Stream.of(x.split("")).mapToInt(Integer::parseInt).max().getAsInt());
 	}
 
 }
